d85b5823f6d41891e1177ae63f62ef07ac1b9fc8
선비 <sunbi2003@gmail.com>
2026-03-27 15:53:17 +0900

    스터디 í생성 페이지 생성

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import './App.css'
 import LoginPage from './pages/LoginPage';
 import SignupPage from './pages/SignupPage';
 import LevelTestPage from './pages/LevelTestPage';
+import MakeStudyPage from './pages/MakeStudyPage';
 
 function App() {
 
@@ -14,6 +15,7 @@ function App() {
           <Route path="/" element={<LoginPage />} />
           <Route path="/signup" element={<SignupPage />} />
           <Route path="/level-test" element={<LevelTestPage />} />
+          <Route path="/make-my-study" element={<MakeStudyPage />} />
         </Routes>
       </BrowserRouter>
     </>
---
+++ src/pages/MakeStudyPage.jsx
@@ -0,0 +1,8 @@
+import React, {useState} from "react";
+
+export default function MakeStudyPage(){
+
+    return(
+        <div> 스터디 생성 페이지</div>
+    );
+}
