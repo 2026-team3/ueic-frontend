9a2414ef65c2a2b14a2a6c60ca3a738268978e13
선비 <sunbi2003@gmail.com>
2026-04-01 18:59:41 +0900

    백엔드 연동 완료(나머지 부분 수정 중)

--- src/pages/MyStudiesPage.jsx
+++ src/pages/MyStudiesPage.jsx
@@ -2,6 +2,7 @@ import React, {useEffect, useState} from "react";
 import Header from "../components/Header";
 import NavigationBar from "../components/NavigationBar";
 import axios from "axios";
+import api from "../apis/axiosInstance.jsx";
 import "../css/MyStudiesPage.css";
 
 export default function MyStudiesPage(){
@@ -17,15 +18,50 @@ export default function MyStudiesPage(){
                         Authorization: `Bearer ${token}`
                     }
                 });
-
                 setStudies(res.data.data);
+                console.log(res.data.data);
+                console.log("token:", token);
+
             } catch (err) {
                 console.error("스터디 목록 조회 실패", err);
             }
         };
-
         fetchMyStudies();
     }, []);
+
+    //스터디 나가기
+    const handleLeave =async (studyId, role) => {
+        try {
+            const token = localStorage.getItem("accessToken");
+            const res = await api.patch(`/studies/${studyId}/leave`);
+            alert(res.data.data);
+
+
+            setStudies((prev) => prev.filter((s) => s.studyId !== studyId));
+        } catch(err) {
+            console.error("스터디 나가기 실패:", err.response?.data || err);
+            alert(err.response?.data?.message || "스터디 나가기 실패");
+
+        }
+    };
+
+    // 스터디 삭제 (팀장만)
+    const handleDelete = async (studyId) => {
+        if (!window.confirm("정말 스터디를 삭제하시겠습니까?")) return;
+
+        try {
+            const token = localStorage.getItem("accessToken");
+            const res = await axios.delete(`/api/studies/${studyId}`, {
+                headers: { Authorization: `Bearer ${token}` },
+            });
+            alert(res.data.data);
+            setStudies((prev) => prev.filter((s) => s.studyId !== studyId));
+        } catch (err) {
+            console.error(err);
+            alert(err.response?.data?.message || "스터디 삭제 실패");
+        }
+    };
+
 
     return(
         <div className="my-studies-list-page">
@@ -35,7 +71,6 @@ export default function MyStudiesPage(){
             </div>
             <div className="my-studies-container">
                 <h1 className="this-page-title">참여 스터디 관리</h1>
-
                 <div className="my-study-card-list">
                     {studies.map((study) => (
                         <div key={study.studyId} className="my-study-card">
@@ -55,12 +90,19 @@ export default function MyStudiesPage(){
 
                             {/* 버튼 */}
                             {study.role === "LEADER" ? (
-                                <div className="my-btn-group">
-                                    <button className="my-leave-btn">나가기</button>
-                                    <button className="my-delete-btn">삭제</button>
-                                </div>
+                                <button
+                                    className="my-delete-btn"
+                                    onClick={()=>handleDelete(study.studyId)}
+                                >
+                                    삭제
+                                </button>
                             ) : (
-                                <button className="my-leave-btn">나가기</button>
+                                <button
+                                    className="my-leave-btn"
+                                    onClick={()=> handleLeave(study.studyId, study.role)}
+                                >
+                                    나가기
+                                </button>
                             )}
                         </div>
                     ))}
